65df7f269f0ddd9b0c9aa48b021793fa1bd46edf
Tien CSE <tien.nguyen11102004@hcmut.edu.vn>
2025-02-16 14:56:09 +0700

    Thingsboard

--- src/RTOS_Task.cpp
+++ src/RTOS_Task.cpp
@@ -1,38 +1,132 @@
 #include "RTOS_Task.h"
-/*------------------------------------------*/
-/*--------------Task------------------------*/
-/*------------------------------------------*/
+/*------------------------------------------------------------------------*/
+/*------------------------Declaration-------------------------------------*/
+constexpr uint32_t MAX_MESSAGE_SIZE = 1024U;      // Kích thước tối đa của thông điệp gửi qua MQTT (1024 bytes)
+constexpr int16_t telemetrySendInterval = 3000U;      // Khoảng thời gian gửi dữ liệu cảm biến lên ThingsBoard (10 giây)
+WiFiClient wifiClient;                            // Đối tượng WiFi để kết nối mạng
+Arduino_MQTT_Client mqttClient(wifiClient);       // Đối tượng MQTT để giao tiếp với broker (ThingsBoard)
+ThingsBoard tb(mqttClient, MAX_MESSAGE_SIZE);     // Đối tượng ThingsBoard để gửi và nhận dữ liệu qua MQTT
+/*-------------------------------------------------------------------------*/
+
+/*-----------------------------Call Back-----------------------------------*/
+
+/*-------------------------------------------------------------------------*/
+CallBack callback;
+
+// Constructor
+CallBack::CallBack(std::vector<const char*> shared_attributes, std::vector<RPC_Callback> rpc_list)
+    : SHARED_ATTRIBUTES_LIST(shared_attributes), RPC_LIST(rpc_list) {}
+
+// Thêm thuộc tính chia sẻ
+void CallBack::Add_Shared_Attribute(const char* shared_attribute) {
+    SHARED_ATTRIBUTES_LIST.push_back(shared_attribute);
+}
+
+// Thêm RPC Callback
+void CallBack::Add_RPC(const char* rpc_name, std::function<RPC_Response(const RPC_Data&)> rpc_response) {
+    RPC_LIST.emplace_back(rpc_name, rpc_response);  // Sử dụng `emplace_back()` tối ưu hơn `push_back()`
+}
+// Khởi tạo CallBack cho các thuộc tính chia sẻ
+void CallBack::Shared_Attribute_Begin(std::function<void*(const Shared_Attribute_Data&)> Shared_callback){
+    this->Shared_callback = Shared_callback;
+}
+// Đăng ký RPC
+void SubscribeRPC(CallBack& callback){
+    if (!tb.RPC_Subscribe(callback.RPC_LIST.cbegin(),callback.RPC_LIST.cend())) {  // Đăng ký nhận lệnh điều khiển từ ThingsBoard (RPC)
+        Serial.println("Failed to subscribe for RPC");
+        return;
+    }
+    const Shared_Attribute_Callback atrributes_callback(callback.Shared_callback,callback.SHARED_ATTRIBUTES_LIST.cbegin(),callback.SHARED_ATTRIBUTES_LIST.cend());
+    const Attribute_Request_Callback attribute_shared_request_callback(callback.Shared_callback,callback.SHARED_ATTRIBUTES_LIST.cbegin(),callback.SHARED_ATTRIBUTES_LIST.cend());
+    if(!tb.Shared_Attributes_Subscribe(atrributes_callback)){ // Đăng ký cập nhật thuộc tính chia sẻ
+        Serial.println("Failed to subcribe for shared attribute updates");
+        return;
+    }
+    if(!tb.Shared_Attributes_Request(attribute_shared_request_callback)){ // Yêu cầu Thingboard gửi lại các thuộc tính hiện tại
+        Serial.println("Failed to request for shared attribute");
+        return;
+    }
+}
+// In danh sách thuộc tính và RPC
+void CallBack::Print_List() {
+    Serial.println("----------- SHARED_ATTRIBUTES_LIST -----------");
+    for (size_t i = 0; i < SHARED_ATTRIBUTES_LIST.size(); i++) {
+        Serial.print(i);
+        Serial.print(" : ");
+        Serial.println(SHARED_ATTRIBUTES_LIST[i]);
+    }
+
+    Serial.println("------------- RPC_LIST -------------");
+    for (size_t i = 0; i < RPC_LIST.size(); i++) {
+        Serial.print(i);
+        Serial.print(" : ");
+        Serial.println(RPC_LIST[i].Get_Name());
+    }
+}
+/*-------------------------------------------------------------------------*/
+
+/*--------------------------------Task-------------------------------------*/
+
+/*-------------------------------------------------------------------------*/
 void TaskBlinky(void* pvParameters){
     INIT_TASK(pvParameters);
+    pinMode(Pin,OUTPUT);
     for(;;){
-        pinMode(Pin,OUTPUT);
         digitalWrite(Pin,HIGH);
         delay(Delay);
         digitalWrite(Pin,LOW);
         delay(Delay);
     }
 }
-void TaskDht(void* pvParameters){
+void TaskLight(void* pvParameters){
     INIT_TASK(pvParameters);
-    DHT_VAL *re_val = (DHT_VAL*)pm.other;
-    uint8_t DHT_type = re_val->DHT_type;
-    DHT_Unified dht(Pin, DHT_type);
-    dht.begin();
+    LIGHT_VAL* light_val = (LIGHT_VAL*) pm.other;
     for(;;){
-        sensors_event_t event;
-        dht.temperature().getEvent(&event);
-        // Serial.print("TEMP: ");
-        // Serial.println(re_val->temperature);
-        re_val->temperature = event.temperature;
-        // Serial.print("HUM: ");
-        // Serial.println(re_val->humidity);
-        dht.humidity().getEvent(&event);
-        re_val->humidity = event.relative_humidity;
-        char trans[50];
+        int light = analogRead(pm.get_Pin());
+        light_val->light= map(light,0,4095,0,100);
         delay(Delay);
     }
 }
-void TaskUart(void* pvParameters){
+void TaskDht(void* pvParameters) {
+    INIT_TASK(pvParameters);
+    DHT_VAL *re_val = (DHT_VAL*)pm.other;
+    uint8_t DHT_type = re_val->DHT_type;
+
+    switch (DHT_type) {
+        case DHT11:
+        case DHT22: {
+            DHT_Unified dht(Pin, DHT_type);
+            dht.begin();
+            for (;;) {
+                sensors_event_t event;
+                dht.temperature().getEvent(&event);
+                re_val->temperature = event.temperature;
+                dht.humidity().getEvent(&event);
+                re_val->humidity = event.relative_humidity;
+                delay(Delay);
+            }
+            break;
+        }
+
+        case Dht20: {
+            DHT20 dht20;
+            Wire.begin(pm.get_Pin(), pm.get_Pin() + 1);  // Khởi tạo giao tiếp I2C với cảm biến DHT20
+            dht20.begin();  
+            for (;;) {
+                dht20.read();
+                re_val->temperature = dht20.getTemperature();
+                re_val->humidity = dht20.getHumidity();
+                delay(Delay);
+            }
+            break;
+        }
+
+        default:
+            break;
+    }
+}
+
+void TaskTransmitUart(void* pvParameters){
     INIT_TASK(pvParameters);
     Uart_VAL transmit = *(Uart_VAL*) pm.other;
     char trans[50];
@@ -40,5 +134,97 @@ void TaskUart(void* pvParameters){
         sprintf(trans,"!1:H:%.2f#!1:T:%.2f#",transmit.dht_val->humidity,transmit.dht_val->temperature);
         Serial.println(trans);
         delay(Delay);
+    }
+}
+void TaskReceiveUart(void* pvParameters){
+    INIT_TASK(pvParameters);
+    pinMode(20,OUTPUT);
+    for(;;){
+        if(Serial.available() > 0){
+            int status = Serial.read() - '0';
+           
+            switch (status)
+            {
+            case 0:
+                digitalWrite(20,LOW);
+                break;
+            case 1:
+                digitalWrite(20,HIGH);
+                break;        
+            default:
+                break;
+            }
+
+        }
+        delay(pm.get_Delay());
+    }
+}
+
+void TaskPublishDataToThingsboard(void* pvParameters){
+    INIT_TASK(pvParameters);
+    ThingsBoard_VAL thingsboard = *((ThingsBoard_VAL*)pm.other);
+    uint8_t num = 0;
+    uint32_t previousDataSend = 0;
+    float longti = 106.76940000  ;
+    float lati = 10.90682000  ;
+    for(;;){
+        delay(10);
+        if(WiFi.status() != WL_CONNECTED){
+            WiFi.begin(thingsboard.WIFI_SSID,thingsboard.WIFI_PASSWORD); // Kết nối esp32 vào WIFI
+            while (WiFi.status()!= WL_CONNECTED)
+            {
+                delay(500);
+                Serial.print(".");
+            }
+            Serial.println("Connected to AP");  // Kết nối thành công
+        }
+        if (!tb.connected()) {  // Kiểm tra kết nối với ThingsBoard, nếu chưa kết nối thì kết nối lại
+                Serial.print("Connecting to: ");
+                Serial.print(thingsboard.THINGSBOARD_SERVER);
+                Serial.print(" with token ");
+                Serial.println(thingsboard.TOKEN);
+                if (!tb.connect(thingsboard.THINGSBOARD_SERVER, thingsboard.TOKEN, thingsboard.THINGSBOARD_PORT)) {  // Kết nối đến ThingsBoard
+                    Serial.println("Connected to ThingsBoard");    
+                    return;   
+                }
+                Serial.println("Subscribing for RPC...");  
+                SubscribeRPC(callback);            
+        }
+        if (millis() - previousDataSend > telemetrySendInterval) {  // Gửi dữ liệu cảm biến định kỳ sau mỗi 10 giây
+            previousDataSend = millis();                
+            float temperature = thingsboard.dht->temperature;  // Lấy dữ liệu nhiệt độ
+            float humidity = thingsboard.dht->humidity;        // Lấy dữ liệu độ ẩm
+
+            if (isnan(temperature) || isnan(humidity)) {  // Kiểm tra nếu cảm biến không trả dữ liệu hợp lệ
+                Serial.println("Failed to read from DHT20 sensor!");
+            } 
+            else {
+                Serial.print("Temperature: ");
+                Serial.print(temperature);
+                Serial.print(" °C, Humidity: ");
+                Serial.print(humidity);
+                Serial.println(" %");
+                
+                tb.sendTelemetryData("temperature", temperature);  // Gửi dữ liệu nhiệt độ lên ThingsBoard
+                tb.sendTelemetryData("humidity", humidity);        // Gửi dữ liệu độ ẩm lên ThingsBoard
+            }
+            if( thingsboard.light_val->light == 0){
+                Serial.println("Falied to read from light sensor");
+            }
+            else{
+                int light = thingsboard.light_val->light;
+                Serial.print("Light: ");
+                Serial.println(light);
+                tb.sendTelemetryData("light", light);
+            }
+
+            // Gửi các thông tin mạng WiFi lên ThingsBoard để theo dõi tình trạng kết nối
+            tb.sendAttributeData("rssi", WiFi.RSSI());  
+            tb.sendAttributeData("channel", WiFi.channel());
+            tb.sendAttributeData("bssid", WiFi.BSSIDstr().c_str());
+            tb.sendAttributeData("localIp", WiFi.localIP().toString().c_str());
+            tb.sendAttributeData("ssid", WiFi.SSID().c_str());
+        }
+        tb.loop();
     }
 }
--- src/RTOS_Task.h
+++ src/RTOS_Task.h
@@ -1,10 +1,21 @@
 #ifndef TASK_H
-#define TASH_H
+#define TASK_H
 #include <Arduino.h>
 #include <DHT.h>
 #include <DHT_U.h>
 #include "string.h"
+#include <WiFi.h>
+#include <Arduino_MQTT_Client.h>
+#include <ThingsBoard.h>
+#include "DHT20.h"
+#include "Wire.h"
+#include <ArduinoOTA.h>
+#include <RTOS_Task.h>
+/*--------------------Define--------------------------------------*/
+#define Dht20 0 
 /*------------------------Macro-----------------------------------*/
+#define setVal(name_val)\
+    this->name_val = name_val
 #define CreateTask(name_task,stack,Pin,Delay,other) \
     Parameter pm_##name_task(Pin,Delay,other); \
     xTaskCreate(Task##name_task, #name_task,stack,(void*) &pm_##name_task,2,NULL);
@@ -14,7 +25,7 @@
     uint32_t Delay = pm.get_Delay();
 /*-------------------------Struct-----------------------------------*/    
 struct LIGHT_VAL{
-
+    int light;
 };
 struct DHT_VAL{
     uint8_t DHT_type;
@@ -24,6 +35,26 @@ struct DHT_VAL{
         this-> DHT_type = DHT_type; 
         this->temperature = 0;
         this->humidity = 0;
+    }
+};
+struct ThingsBoard_VAL{
+    DHT_VAL* dht;
+    LIGHT_VAL* light_val;
+    const char* WIFI_SSID;
+    const char* WIFI_PASSWORD;
+    const char* THINGSBOARD_SERVER;
+    const char* TOKEN;
+    uint16_t THINGSBOARD_PORT;
+    
+    ThingsBoard_VAL(DHT_VAL* dht,LIGHT_VAL* light,const char* WIFI_SSID,const char* WIFI_PASSWORD,
+        const char* THINGSBOARD_SERVER,const char* TOKEN,uint16_t THINGSBOARD_PORT){
+        this->dht = dht;
+        this->light_val = light;
+        this->WIFI_SSID = WIFI_SSID;
+        this->WIFI_PASSWORD = WIFI_PASSWORD;
+        setVal(TOKEN);
+        setVal(THINGSBOARD_SERVER);
+        setVal(THINGSBOARD_PORT);
     }
 };
 struct Uart_VAL{
@@ -53,7 +84,33 @@ class Parameter{
             return this->Task_Delay;
         }
 };
+/*-----------------------CallBack------------------------*/
+class CallBack {
+    private:
+        std::vector<const char*> SHARED_ATTRIBUTES_LIST; // Danh sách thuộc tính chia sẻ
+        std::vector<RPC_Callback> RPC_LIST; // Danh sách RPC
+        std::function<void*(const Shared_Attribute_Data&)> Shared_callback;
+        // Đăng kí RPC
+        friend void SubscribeRPC(CallBack& callback);
+    public:
+        // Constructor
+        CallBack(std::vector<const char*> shared_attributes = {}, std::vector<RPC_Callback> rpc_list = {});
+
+        // Thêm một thuộc tính chia sẻ
+        void Add_Shared_Attribute(const char* shared_attribute);
+        // Khởi tạo hàm CallBack cho các thuộc tính chia sẻ
+        void Shared_Attribute_Begin(std::function<void*(const Shared_Attribute_Data&)> Shared_callback);
+        // Thêm một RPC mới
+        void Add_RPC(const char* rpc_name, std::function<RPC_Response(const RPC_Data&)> rpc_response);
+        // In danh sách thuộc tính và RPC đã đăng ký
+        void Print_List();
+};
+extern CallBack callback;
+/*------------------------Task-------------------------------*/
+void TaskLight(void* pvParameters);
 void TaskBlinky(void* pvParameters);
 void TaskDht(void* pvParameters);
-void TaskUart(void* pvParameters);
+void TaskTransmitUart(void* pvParameters);
+void TaskReceiveUart(void* pvParameters);
+void TaskPublishDataToThingsboard(void* pvParameters);
 #endif
